b3a050cfa8c7e34477c482e6ab88a4009be9d8a4
jaypandey0 <jaypandey0@gmail.com>
2022-09-22 16:43:21 +0530

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Enter the number");
+            i = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadLine();
+        }
+    }
+}
